41d5fbe914afedc753cc3265d3dac112a0335a4a
Takeshi <tamashi.kodokuna148@gmail.com>
2023-09-29 23:56:15 +0700

    add time input + try styled varibles
    max is 24 hours and 60 minutes

---
+++ components/styled.js
@@ -0,0 +1,8 @@
+export const inputTime = {
+  // background: "#1D1D1D",
+  textAlign: "center",
+  width: "2rem",
+  height: "100%",
+  border: "1px solid lightgray",
+  borderRadius: "5px 10px",
+};
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,28 +1,73 @@
-import Image from 'next/image'
-
+import Image from "next/image";
+import { useState } from "react";
+import { inputTime } from "../components/styled";
 export default function Home() {
+  const [filterHour, setFilterHour] = useState();
+  const [filterMinute, setFilterMinute] = useState();
+  // const [value, setValue] = useState();
+
+  const handleHourChange = (event) => {
+    const hour = event.target.value;
+    if (hour >= 0 && hour <= 24) {
+      setFilterHour(parseInt(hour));
+      hour == 0 ? setFilterHour(0) : {};
+    }
+  };
+
+  const handleMinuteChange = (event) => {
+    const minute = event.target.value;
+    if (minute >= 0 && minute <= 60) {
+      setFilterMinute(parseInt(minute));
+      minute == 0 ? setFilterMinute(0) : {};
+    }
+  };
+  // const handleChange = (event) => {
+  //   setValue(parseInt(event.target.value));
+  // };
+  // const filteredTasks = tasks.filter((task) => {
+  //   const totalMinutes = filterHour * 60 + filterMinute;
+  //   return task.completionTime < totalMinutes;
+  // });
   return (
     <>
-      <div class="m-auto  max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <a href="#">
-      <Image src="/Shadow Garden.png" alt="image" width={1000} height={600} />
-    </a>
-    <div class="p-5">
-        <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-        </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Read more
-             <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-            </svg>
-        </a>
-    </div>
-</div>
+      <main className="">
+        <section
+          id="input-time"
+          className="m-auto text-center text-2xl font-bold"
+        >
+          <h2>How many time do you have left</h2>
+          <div id="time" className="">
+            <input
+              style={inputTime}
+              type="text"
+              value={filterHour}
+              onChange={handleHourChange}
+              placeholder="00"
+              className="text-center"
+              maxLength={2}
+            />
+            <span>:</span>
+            <input
+              style={inputTime}
+              type="text"
+              maxLength={2}
+              value={filterMinute}
+              onChange={handleMinuteChange}
+              placeholder="00"
+              className="text-center"
+            />
+            {/* <input
+              type="number"
+              value={value}
+              onChange={handleChange}
+              placeholder="test"
+            /> */}
+          </div>
+        </section>
+      </main>
     </>
-  )
+  );
 }
 // i think this is bad practice, but i don't know what best practice is
 // set static title
-Home.title = "Home"
+Home.title = "Home";
